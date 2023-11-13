f7bedd967e3cbd23b27d1a99c361dcb6d8773068
funtrue <bei_ming_fugue@qq.com>
2023-11-13 16:56:14 +0800

    完善后端settingsetting

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1itppe5^7oulf%t4$!6!wcoske$tujxk4+213)t$26(xo%a35a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
